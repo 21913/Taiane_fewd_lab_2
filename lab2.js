d8c50203d1f252b3253d1464339f086b2caeb2d0
21913 <21913@student.dorset-college.ie>
2021-01-27 20:00:46 +0000

    commit

---
+++ lab2.js
@@ -0,0 +1,27 @@
+//Taiane Vieira da Silva - ID 21913
+
+var myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
+        console.log(myAlphabet.length);
+    
+//5 and 6
+const Planets = ['Earth', 'Mercury', 'Venus', 'Mars', 'Jupiter', 'Uranus'];
+console.log(Planets[0]);
+console.log(Planets[1]);
+console.log(Planets[2]);
+console.log(Planets[3]);
+console.log(Planets[4]);
+
+// 7
+const wowDatatypes = [3, 'home', false];
+var x = wowDatatypes[0];
+var y = wowDatatypes[1];
+var z = wowDatatypes[2];
+console.log(x,y,z);
+
+//11 and 12
+var hello = function(){
+    return 'Hello World!';
+    };
+    console.log(hello());
+
+
